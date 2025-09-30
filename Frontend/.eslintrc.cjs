e64b89aa18d1036f3e7f23df744e23e6b5593835
NadeeshaMedagama <nadeeshamedaga@gmail.com>
2025-09-30 09:31:19 +0530

    Fix frontend ESLint configuration
    - Remove TypeScript ESLint dependencies
    - Use standard JavaScript parser options
    - Fix configuration for React JSX support
    - Resolve missing config dependencies error

--- Frontend/.eslintrc.cjs
+++ Frontend/.eslintrc.cjs
@@ -6,14 +6,19 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    '@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: [
     'dist', 
     '.eslintrc.cjs'
   ],
-  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
