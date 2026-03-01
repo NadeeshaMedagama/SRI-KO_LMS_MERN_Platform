05ec7a3a38048ba02e14d900fee47f28acc357e1
NadeeshaMedagama <nadeeshamedagama@gmail.com>
2026-03-01 22:17:06 +0530

    eslint config version updated

---
+++ Frontend/eslint.config.js
@@ -0,0 +1,80 @@
+import js from '@eslint/js';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+
+export default [
+  // Ignore patterns
+  {
+    ignores: ['dist/**'],
+  },
+
+  // Base JS recommended rules
+  js.configs.recommended,
+
+  // Main config for JS/JSX files
+  {
+    files: ['**/*.{js,jsx}'],
+    plugins: {
+      react: reactPlugin,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.es2020,
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      // React plugin recommended rules
+      ...reactPlugin.configs.recommended.rules,
+
+      // React Hooks recommended rules
+      ...reactHooks.configs.recommended.rules,
+
+      // React Refresh
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+
+      // General rules
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-undef': 'error',
+      'prefer-const': 'error',
+      'no-var': 'error',
+      'object-shorthand': 'error',
+      'prefer-template': 'error',
+      'template-curly-spacing': 'error',
+      'arrow-spacing': 'error',
+      'comma-dangle': ['error', 'always-multiline'],
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      indent: 'off',
+      'no-trailing-spaces': 'error',
+      'eol-last': 'error',
+      'jsx-quotes': ['error', 'prefer-double'],
+
+      // React JSX transform (React 17+)
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+    },
+  },
+];
+
